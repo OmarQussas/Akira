6c82a3461b67a65c10ed3e5e40dc5f2d6239e52c
OmarQussas <94117660+OmarQussas@users.noreply.github.com>
2025-06-17 14:44:46 +0300

    Add files via upload

---
+++ akira/problems.js
@@ -0,0 +1,25 @@
+
+// function add(x,y){
+//     return ` The sum is ${x + y}` 
+// }
+
+
+// function increment(a){
+//     return `the incremented number is ${++a}`
+// }
+
+// // revise
+// function incrementArray(array){
+      
+
+//       array[array.length - 1] = array[array.length - 1 ] + 1
+//      return array 
+// } 
+
+// function yearsToDays(year){
+//     return year * 365 
+// }
+
+
+
+
